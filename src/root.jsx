f451b371170631068bd3053216ba311e17423591
Francis Anyuru <anyuruf@gmail.com>
2025-10-26 11:39:44 -0400

    fix prettier config and some meta

--- src/root.jsx
+++ src/root.jsx
@@ -17,7 +17,7 @@ export function Layout({
           name="viewport"
           content="width=device-width, initial-scale=1.0"
         />
-        <title>My App</title>
+        <title>OpenStream</title>
         <Meta />
         <Links />
       </head>
